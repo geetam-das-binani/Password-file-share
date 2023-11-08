adc048efcd56ae2d12bc3c28284f8d21bfb086a0
Geetam <geetambinani6@gmail.com>
2023-11-08 22:41:03 +0530

    add port in env file

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 import express from "express";
 import { router } from "./routes/route.mjs";
 import { connect } from "./Database/Connection.mjs";
-import { engine } from 'express-handlebars';
+import { engine } from "express-handlebars";
+const PORT = process.env.PORT || 4501;
 const app = express();
-connect(); 
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars'); 
- 
+connect();
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
-	res.render("index");  
-}); 
-app.use("/", router);  
+  res.render("index");
+});
+app.use("/", router);
 
-app.listen(8000, () => console.log("Server is listening on port 8000"));
- 
+app.listen(PORT, () => console.log("Server is listening on port 8000"));
